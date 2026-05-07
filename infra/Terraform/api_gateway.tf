f91cea127d47c1793872da0f047babc544849608
Ronan-EruiniBennett <rheb99@icloud.com>
2026-05-07 15:03:09 +1000

    lambda working and pushing data to s3 correctly

--- infra/Terraform/api_gateway.tf
+++ infra/Terraform/api_gateway.tf
@@ -20,7 +20,7 @@ resource "aws_apigatewayv2_authorizer" "APIAuthorizer" {
   authorizer_type                  = "JWT"
   name                             = "Cognito_Token_Authorizer"
   authorizer_result_ttl_in_seconds = 0
-  identity_sources = [ "$request.header.Authorization" ]
+  identity_sources                 = ["$request.header.Authorization"]
 
   jwt_configuration {
     audience = [aws_cognito_user_pool_client.Gymlogbook_user_pool_client.id]
--- infra/Terraform/lambda.tf
+++ infra/Terraform/lambda.tf
@@ -16,8 +16,8 @@ data "aws_iam_policy_document" "trust_policy_gym_function" {
     effect = "Allow"
 
     principals {
-      type = "Service"
-      identifiers = [ "lambda.amazonaws.com" ]
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
@@ -49,6 +49,16 @@ resource "aws_iam_role_policy" "add_permissions" {
 resource "aws_iam_role_policy_attachment" "add_logging" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_gym_log_execution_role.id
+}
+
+// Adding lambda resource policy to allow invokes from api gateway http api
+resource "aws_lambda_permission" "lambda_resource_policy" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.gym_log_csv_function.id
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.httpAPI.execution_arn}/*"
 }
 
 // Create the Lambda function resource, uses sha256 to discover changes in source file, creates environment variable BUCKET_NAME for the lambda function to use.
